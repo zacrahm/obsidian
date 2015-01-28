c5e88b12fdf151d79922c93bfd5a77e5fb9881de
rdemase <robbyd32@vt.edu>
2015-01-27 20:52:25 -0500

    Simple Level Setup
    Crappy scrolling LED banner, materials, and script; basic obsidian
    reflective material; basic buildings; level setup; organized assets

---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float offset = Time.time * 0.5f;
+		renderer.material.SetTextureOffset ("_MainTex", new Vector2 (offset, 0));
+	}
+}
